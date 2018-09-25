127131a84ac5ca921a5287de1f4831489e3a325c
priyankvex <priyankvex95@gmail.com>
2018-09-25 12:27:07 +0530

    Edits in deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,6 @@
 sudo apt-get install python-virtualenv;
-virtualenv --python=python3.6 myvenv;
+virtualenv --python=python3.6 priyank-infilect-vnev;
 sudo apt install python3.6-venv;
-python3.6 -m venv /priyank-infilect-vnev;
 source priyank-infilect-vnev/bin/activate && pip install -r requirements.txt &&
 python manage.py migrate &&
 python manage.py runserver
